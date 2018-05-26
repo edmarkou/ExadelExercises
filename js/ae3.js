9fbeb74d486a8c3245a84e7ea579f6b753e06f08
edmarkou <edvardas.markevicius@gmail.com>
2018-05-26 04:00:32 +0300

    Modified ae3 and index

--- js/ae3.js
+++ js/ae3.js
@@ -1,14 +1,30 @@
-const $addRectangle = $('#addRectangle');
-const $canvas = $('#myCanvas');
+const addRectangle = document.getElementById('addRectangle');
+const fillCanvas = document.getElementById('fillCanvas');
+const canvas = document.getElementById('myCanvas');
 
-$addRectangle.on('click', ()=>{
-    if ($canvas.getContext) {
-        var ctx = $canvas.getContext('2d');
+addRectangle.addEventListener('click', ()=>{
+    let ctx = canvas.getContext('2d');
 
-        ctx.fillRect(25, 25, 100, 100);
-        ctx.clearRect(45, 45, 60, 60);
-        ctx.strokeRect(50, 50, 50, 50);
-    }
+    //random parameters for rectangle
+    let y = Math.floor(Math.random() * 600);
+    let x = Math.floor(Math.random() * 1500);
+    let width = Math.floor(Math.random() * 1500);
+    let height = Math.floor(Math.random() * 650);
+
+    //random color parameters
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 200);
+
+
+    ctx.fillStyle = 'rgba('+r+', '+g+', '+b+', 1)';
+    ctx.fillRect(x, y, width, height);
+
+});
+
+fillCanvas.addEventListener('click', ()=>{
+    //ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     //fillRect(x, y, width, height)
 });
+
 
