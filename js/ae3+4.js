e46e1d485e7db640f8a17fef0e851e0fba5a95ba
edmarkou <edvardas.markevicius@gmail.com>
2018-05-31 02:13:04 +0300

    Finished 3rd exercise, 4th half-finished

--- js/ae3+4.js
+++ js/ae3+4.js
@@ -32,7 +32,7 @@ addRectangle.addEventListener('click', ()=>{
         if(goodToGo){
             ctx.fillStyle = getRGBA();     //start drawing the rectangle
             ctx.fillRect(x, y, width, height);
-            blockedSpace.push([x, y, width, height]);
+            blockedSpace.push([x, y, width, height]);       //stores the new rectangles coordinates
         }
     }while(goodToGo === false);
 });
@@ -74,7 +74,7 @@ fillCanvas.addEventListener('click', ()=>{
         if(!ctx.getImageData(0, 0, canvasWidth, canvasHeight).data.includes(0)) {
             clearInterval(i);
         }
-    }, 200);
+    }, 200);                                                    //fills the canvas 1 rectangle per 0.2 second until its full
 });
 
 function rect_collision(x1, y1, width1, height1, x2, y2, width2, height2) {
@@ -88,11 +88,11 @@ function rect_collision(x1, y1, width1, height1, x2, y2, width2, height2) {
     top2 = y2;
     bottom2 = y2 + height2;
     return !(left1 >= right2 || left2 >= right1 || top1 >= bottom2 || top2 >= bottom1);
-}
+}                                                               //checks if two rectangles collide with each other
 
 function getRGBA(){
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 200);
+    let r = Math.floor(Math.random() * 255) + 1;
+    let g = Math.floor(Math.random() * 255) + 1;
+    let b = Math.floor(Math.random() * 200) + 1;
     return 'rgba('+r+', '+g+', '+b+', 1)';
-}
+}                                                               //returns random color
